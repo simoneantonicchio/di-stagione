a2f4872e3de6782a32cac6f420141d48d18f99bb
Lidia Cirrone <lidia.cirrone@gmail.com>
2024-06-15 19:16:45 +0200

    added log inside forceGetInfoCall cron job

--- src/cronjobs/cronjobs.service.ts
+++ src/cronjobs/cronjobs.service.ts
@@ -1,14 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { Cron } from '@nestjs/schedule';
 import { AppService } from 'src/app.service';
 
 @Injectable()
 export class CronjobsService {
   constructor(private readonly appService: AppService) {}
+  private readonly logger = new Logger(CronjobsService.name);
 
   // forces a getInfo call every 14 minutes between 7am and midnight (local time)
-  @Cron('0 */14 7-23 * * *')
+  @Cron('0 */14 7-23 * * *', {
+    timeZone: 'Europe/Rome',
+  })
   forceGetInfoCall() {
+    this.logger.debug('called forceGetInfoCall()');
     this.appService.getInfo();
   }
 }

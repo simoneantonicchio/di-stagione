9e831b1cb8132083e6214fbbdd34920842715156
Lidia Cirrone <lidia.cirrone@gmail.com>
2024-06-19 14:09:13 +0200

    changed cron job time to every 10 minutes, all day

--- src/cronjobs/cronjobs.service.ts
+++ src/cronjobs/cronjobs.service.ts
@@ -9,8 +9,8 @@ export class CronjobsService {
   constructor(private readonly httpService: HttpService) {}
   private readonly logger = new Logger(CronjobsService.name);
 
-  // forces a getInfo call every 10 minutes between 7am and midnight (local time)
-  @Cron('0 */10 7-23 * * *', {
+  // forces a getInfo call every 10 minutes
+  @Cron('0 */10 * * * *', {
     timeZone: 'Europe/Rome',
   })    
   async forceGetInfoCall(): Promise<any> {
